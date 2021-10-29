5c9b9cc16e99a3aa6df77ac46780daa34453a31d
lucenafel <lucena.felipe@hotmail.com>
2021-10-30 01:11:16 +0200

    tests

--- src/main.c
+++ src/main.c
@@ -6,35 +6,54 @@
 /*   By: lfelipe- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 19:01:41 by lfelipe-          #+#    #+#             */
-/*   Updated: 2021/10/26 22:13:22 by lfelipe-         ###   ########.fr       */
+/*   Updated: 2021/10/30 01:10:53 by lfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
 	int pid;
-	// int fd[2];
-	int i = 0;
-	char buf[2];
+	int fd[2];
+	char buf[5];
+	char *pin;
 
+	printf("%d\n", X_OK);
+	printf("%d\n", W_OK);
+	printf("%d\n", R_OK);
+
+	if (!access("/usr/bin/", 1))
+		printf("OK\n");
+	else
+		printf("FAIL\n");
+
+	if (pipe(fd) == -1)
+	{
+		perror("PIPE ERROR");
+		exit(EXIT_FAILURE);
+	}
 	pid = fork();
+
 
 	if (pid == 0)
 	{
-		i = 10;
-		printf("%d\n", i);
+		pin = "4123\0";
+		close(fd[0]);
+		write(fd[1], pin, 5);
+		printf("Generation PID PIN\n");
 	}
-	else
+	if (pid > 0)
 	{
-		printf("%d\n", i);
-		printf("%s\n", buf);
+		wait(NULL);
+		close(fd[1]);
+		read(fd[0], buf, 5);
+		close(fd[0]);
+		printf("PIN ID: %s\n", buf);
 	}
-	fork();
-	printf("TESTE 2\n");
-	fork();
-	printf("TESTE 3\n");
+	return (EXIT_SUCCESS);
 	
 }
