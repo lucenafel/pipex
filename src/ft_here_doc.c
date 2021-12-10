d622ed5335ef493a93827042d268c14338eab86d
lucenafel <lucena.felipe@hotmail.com>
2021-12-10 19:40:26 -0300

    doc fix

--- src/ft_here_doc.c
+++ src/ft_here_doc.c
@@ -6,7 +6,7 @@
 /*   By: lfelipe- <lfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 01:32:38 by lfelipe-          #+#    #+#             */
-/*   Updated: 2021/12/08 17:57:08 by lfelipe-         ###   ########.fr       */
+/*   Updated: 2021/12/10 19:37:28 by lfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ void	ft_here_doc(char *deli, int *fd)
 {
 	char	*ret;
 	size_t	len;
+	size_t	d_len;
 
 	close(fd[0]);
+	d_len = ft_strlen(deli);
 	while (1)
 	{
 		write(1, "here_doc> ", 10);
 		ret = get_next_line(0);
 		len = ft_strlen(ret);
-		if (!ft_strncmp(ret, deli, len - 1))
+		if ((len -1 == d_len) && !ft_strncmp(ret, deli, len - 1))
 		{
 			close(fd[1]);
 			free(ret);
